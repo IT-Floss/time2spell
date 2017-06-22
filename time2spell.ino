bacb1e0b57418723e2a49e0761df3bc2a96f9bf5
SebaMinguez <sebaminguez@gmail.com>
2017-06-22 02:31:26 -0300

    fisrt useful code

--- time2spell.ino
+++ time2spell.ino
@@ -1,18 +1,18 @@
 /*
-Description:
+  Description:
 
 
-Using LCD Keypad Field v1.0 https://www.dfrobot.com/wiki/index.php/Arduino_LCD_KeyPad_Shield_(SKU:_DFR0009)
+  Using LCD Keypad Field v1.0 https://www.dfrobot.com/wiki/index.php/Arduino_LCD_KeyPad_Shield_(SKU:_DFR0009)
 
-Listado de pines utilizados del Arduino:
-4: LCD DB4 (pin 11 LCD)
-5: LCD DB5 (pin 12 LCD)
-6: LCD DB6 (pin 13 LCD)
-7: LCD DB7 (pin 14 LCD)
-8: LCD RS (pin 4 LCD) 
-9: LCD Enable (pin 6 LCD)
-10: LCD BackLight
-A0: read buttons
+  Listado de pines utilizados del Arduino:
+  4: LCD DB4 (pin 11 LCD)
+  5: LCD DB5 (pin 12 LCD)
+  6: LCD DB6 (pin 13 LCD)
+  7: LCD DB7 (pin 14 LCD)
+  8: LCD RS (pin 4 LCD)
+  9: LCD Enable (pin 6 LCD)
+  10: LCD BackLight
+  A0: read buttons
 
 */
 
@@ -33,84 +33,109 @@ int adc_key_in  = 0;
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
 // read the buttons
-int read_LCD_buttons()
-{
- adc_key_in = analogRead(0);      // read the value from the sensor 
- // my buttons when read are centered at these valies: 0, 144, 329, 504, 741
- // we add approx 50 to those values and check to see if we are close
- if (adc_key_in > 1000) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
- // For V1.1 us this threshold
- if (adc_key_in < 50)   return btnRIGHT;  
- if (adc_key_in < 250)  return btnUP; 
- if (adc_key_in < 450)  return btnDOWN; 
- if (adc_key_in < 650)  return btnLEFT; 
- if (adc_key_in < 850)  return btnSELECT;  
+int read_LCD_buttons() {
+  adc_key_in = analogRead(0);      // read the value from the sensor
+  // we add approx 50 to those values and check to see if we are close
+  if (adc_key_in > 1000) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
 
- // For V1.0 comment the other threshold and use the one below:
-/*
- if (adc_key_in < 50)   return btnRIGHT;  
- if (adc_key_in < 195)  return btnUP; 
- if (adc_key_in < 380)  return btnDOWN; 
- if (adc_key_in < 555)  return btnLEFT; 
- if (adc_key_in < 790)  return btnSELECT;   
-*/
+  if (adc_key_in < 50)   return btnRIGHT;
+  if (adc_key_in < 155)  return btnUP;
+  if (adc_key_in < 300)  return btnDOWN;
+  if (adc_key_in < 455)  return btnLEFT;
+  if (adc_key_in < 700)  return btnSELECT;
 
- return btnNONE;  // when all others fail, return this...
+  return btnNONE;  // when all others fail, return this...
 }
 
-void setup() { 
+// Software variables
+int time2talk = 40;
+int time2ask = 15;
+int timeNow = 0; //actual time in minutes
+int timePas = -1; //actual time in minutes
+int time2change = 0;
+int progressBar = 0; //Q chars
+String txtMsg = "";
+int aux = 0;
 
-// inicializar display LCD en 16 columnas, 2 filas
+void setup() {
+  // inicializar display LCD en 16 columnas, 2 filas
   lcd.begin(16, 2);
-} 
+  lcd.setCursor(0, 0);
+  lcd.print("   time2spell  ");
+  analogWrite(10, 128); //set backlight
+}
 
 void loop() {
- lcd.setCursor(3,0);            // move cursor to first line
- lcd.print(millis()/1000);      // display seconds elapsed since power-up
- lcd.print(millis()/1000);      // display seconds elapsed since power-up
+  timeNow = millis() / 60000; // divided by 60000 for convert to minutes. decrease to speed testing
 
- lcd.setCursor(0,1);            // move to the begining of the second line
- lcd_key = read_LCD_buttons();  // read the buttons
+  if ( timeNow != timePas ) { //each minute do:
+    lcd.setCursor(0, 0);
+    lcd.print(timeNow);
 
- switch (lcd_key)               // depending on which button was pushed, we perform an action
- {
-   case btnRIGHT:
-     {
-     lcd.print("RIGHT ");
-     break;
-     }
-   case btnLEFT:
-     {
-     lcd.print("LEFT   ");
-     break;
-     }
-   case btnUP:
-     {
-     lcd.print("UP    ");
-     break;
-     }
-   case btnDOWN:
-     {
-     lcd.print("DOWN  ");
-     break;
-     }
-   case btnSELECT:
-     {
-     lcd.print("SELECT");
-     break;
-     }
-     case btnNONE:
-     {
-     lcd.print("NONE  ");
-     break;
-     }
-     
- }
-lcd.print(adc_key_in); 
+    txtMsg = time2talk - timeNow;
+    txtMsg = " " + txtMsg;
 
+    lcd.setCursor(16 - txtMsg.length() , 0);
+    lcd.print(txtMsg);
+  }
+  timePas = timeNow;
+
+  // draw progression Bar
+  if ( timeNow > time2change ) {
+    lcd.setCursor(progressBar, 1); // move cursor to second line, and progress char
+    lcd.print(char(255));      // print progress char
+
+    progressBar++;
+    time2change = progressBar * time2talk / 16; //new threshold
+  }
+
+  if ( timeNow > time2talk ) { // what do when time is over
+    lcd.setCursor(0, 0);
+    lcd.print("  charm's over  ");
+    lcd.setCursor(0, 1);
+    lcd.print("becoming pumpkin");
+    while (true) {
+      delay(100000);
+    }
+  }
+
+  /*
+    switch (lcd_key)               // depending on which button was pushed, we perform an action
+    {
+    case btnRIGHT:
+  	  {
+        lcd.print("RIGHT    ");
+        break;
+      }
+    case btnLEFT:
+  	  {
+        lcd.print("LEFT   ");
+        break;
+      }
+    case btnUP:
+  	  {
+        lcd.print("UP     ");
+        break;
+      }
+    case btnDOWN:
+  	  {
+        lcd.print("DOWN   ");
+        break;
+      }
+    case btnSELECT:
+  	  {
+        lcd.print("SELECT ");
+        break;
+      }
+  	       case btnNONE:
+       {
+       lcd.print("NONE  ");
+       break;
+       }
+
+    }
+    if (lcd_key != btnNONE) {
+  	lcd.print(adc_key_in);
+    }
+  */
 }
-
-
-
-
-
